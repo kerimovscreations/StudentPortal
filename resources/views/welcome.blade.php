b7a682e1a99c6fd8b4feb5ff431002036f52cc79
Karim Karimov <kerimovscreations@gmail.com>
2016-05-18 14:56:35 +0400

    Compressed images, make wait welcome page until resource loading.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div layout="column" layout-align="center center"
-         style="width:100%; height: 100%; background-image: url('{{ URL::to('/') }}/images/welcome_background.jpg'); background-size: cover">
+         style="width:100%; height: 100%; background-image: url('{{ URL::to('/') }}/images/welcome_background.jpg'); background-size: cover" ng-cloak>
         <img src="{{URL::to('/')}}/images/ca_logo.png" style="width: 400px">
         <div layout="row" layout-align="center" style="margin-top: 40px">
             <md-button class="md-raised" ng-click="changePath('login')" onclick="location.href='/login'">Login
