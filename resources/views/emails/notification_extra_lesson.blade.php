437ee01992ea3816d6fd7a90d1c14106ba3e0f10
Karim Karimov <kerimovscreations@gmail.com>
2016-06-12 22:34:07 +0400

    Added links on email footer, fixes style of event add dialog.

--- resources/views/emails/notification_extra_lesson.blade.php
+++ resources/views/emails/notification_extra_lesson.blade.php
@@ -49,12 +49,15 @@
 
 <div><span style="color:#696969;"><span
                 style="font-family:lucida sans unicode,lucida grande,sans-serif;">Send from</span></span><span
-            style="color:#696969;"><span
-                style="font-family:lucida sans unicode,lucida grande,sans-serif;"> </span></span><span
-            style="color:#FFA07A;"><span style="font-family:lucida sans unicode,lucida grande,sans-serif;"><strong>Student
-                Portal</strong> </span></span><span style="color:#696969;"><span
-                style="font-family:lucida sans unicode,lucida grande,sans-serif;">by </span></span><span
-            style="color:#97D1D5;"><span
-                style="font-family:lucida sans unicode,lucida grande,sans-serif;">Code Academy</span></span></div>
+            style="color:#696969;">
+        <span style="font-family:lucida sans unicode,lucida grande,sans-serif;"> </span></span>
+
+    <a href="http://portal.code.edu.az/"
+       style="text-decoration: none;color:#FFA07A;font-family:lucida sans unicode,lucida grande,sans-serif;"><span><strong>StudentPortal</strong> </span></a>
+    <span style="color:#696969;">
+        <span style="font-family:lucida sans unicode,lucida grande,sans-serif;">by </span></span>
+    <a href="http://code.edu.az/"
+       style="color:#97D1D5;text-decoration: none;font-family:lucida sans unicode,lucida grande,sans-serif">
+        <span>Code Academy</span></a></div>
 </body>
 </html>
